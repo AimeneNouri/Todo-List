50535ea5f2fa302e0fe15158b401aa608a934eab
Aimene <aimennouri65@gmail.com>
2020-03-26 23:23:33 +0100

    building the app

--- TodoListApp/src/App.vue
+++ TodoListApp/src/App.vue
@@ -1,60 +1,51 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+  <div id="todolistapp">
+    <div class="container">
+      <h1>Todo Vue App</h1>
+    <input class="input" v-model="ToDo" placeholder="enter a name">
+    <button class="add-btn" @click="add()">Add</button>
+
+    <ul style="list-style-type: none;">
+    <li v-for="(todo,i) in ToDoExisting">
+      <span>
+        <strong>{{i + 1}}</strong> => {{  todo.name  }}
+      </span>
+      <button class="add-btn" @click="deletetodo(i)">Remove</button>
+    </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'todolistapp',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ToDo: '',
+      ToDoExisting:
+      [
+        {name: 'Aimene', id:0},
+        {name: 'Ikram', id:1},
+        {name: 'Hassan', id:2},
+      ]
+    }
+  },
+  methods: {
+    add(){
+      this.ToDoExisting.push({
+        name: this.ToDo,
+        id: new Date().valueOf()
+      }),
+      this.ToDo=''
+    },
+    deletetodo(i){
+      this.ToDoExisting.splice(i,1)
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
