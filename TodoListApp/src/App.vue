420354c445ea0a553cba2be32a33e8bfc39f7e9c
Aimene <aimennouri65@gmail.com>
2020-03-27 12:58:06 +0100

    final update

--- TodoListApp/src/App.vue
+++ TodoListApp/src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div id="todolistapp">
-    <div class="container">
-      <h1>Todo Vue App</h1>
-        <input
-        type="input" class="input" placeholder="Name" required
-        />
-      <button class="add-btn" @click="add()">Add Todo</button>
-
-      <ul>
-        <li v-for="(todo, i) in ToDoExisting">
-          <span>
-            <strong>{{ i + 1 }}</strong> : {{ todo.name }}
-          </span>
-          <button class="delete-btn" @click="deletetodo(i)">X</button>
-        </li>
-      </ul>
+  <div :style="{backgroundImage:'url()'}">
+    <div id="todolistapp">
+      <div class="container">
+        <h1>Todo Vue App</h1>
+          <input
+          type="input" class="input" placeholder="Name" required v-model="ToDo"
+          />
+        <button class="add-btn" @click="add()">Add Todo</button>
+        <ul>
+          <li v-for="(todo, i) in ToDoExisting">
+            <span>
+              <strong>{{ i + 1 }}</strong> : {{ todo.name }}
+            </span>
+            <button class="delete-btn" @click="deletetodo(i)">X</button>
+          </li>
+        </ul>
+              <p class="footer">Build with ❤️ By <a href="https://github.com/AimeneNouri" target="_blank">Aimene Nouri</a> Using <a href="https://vuejs.org/" target="_blank">Vue.js</a></p>
+      </div>
     </div>
   </div>
 </template>
@@ -25,13 +27,13 @@ export default {
   name: "todolistapp",
   data() {
     return {
-      ToDo: "",
+      ToDo: '',
       ToDoExisting: [
         { name: "Aimene", id: 0 },
         { name: "Ikram", id: 1 },
         { name: "Hassan", id: 2 }
       ]
-    };
+    }
   },
   methods: {
     add() {
@@ -39,20 +41,24 @@ export default {
         name: this.ToDo,
         id: new Date().valueOf()
       }),
-        (this.ToDo = "");
+        this.ToDo = ''
     },
     deletetodo(i) {
-      this.ToDoExisting.splice(i, 1);
+      this.ToDoExisting.splice(i,1);
     }
   }
 };
 </script>
 
 <style>
-#todolistapp {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+body {
+  font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: calc(10px + 0.33vw);
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  padding: 5vh 10vw;
+  color: #2c3e50;
+}
+#todolistapp {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
@@ -73,6 +79,7 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+  font-size: 20px;
 }
 input {
   width: 50%;
@@ -82,18 +89,13 @@ input {
   border-radius: 25px;
 }
 .add-btn {
-  /* default for <button>, but useful for <a> */
   display: inline-block;
   text-align: center;
   text-decoration: none;
-  /* create a small space when buttons wrap on 2 lines */
   margin: 2px 0;
-  /* invisible border (will be colored on hover/focus) */
   border: solid 1px transparent;
   border-radius: 4px;
-  /* size comes from text & padding (no width/height) */
   padding: 0.5em 1em;
-  /* make sure colors have enough contrast! */
   color: #ffffff;
   background-color: #9555af;
   border-radius: 25px;
@@ -122,14 +124,41 @@ input {
   border-radius: 25px;
 }
 .delete-btn:hover {
-  color: #fff;
+  color: red;
   border-color: currentColor;
-  background-color: red;
+  background-color: white;
 }
 .delete-btn {
   outline: none;
 }
 .delete-btn::-moz-focus-inner {
   border: none;
+}
+.footer{
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  font-size: 20px;
+  font-weight: bold;
+}
+a {
+  text-decoration: none;
+  color: #2c3e50;
+  position: relative;
+  }
+a:after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  top: 60%;
+  left: -0.1em;
+  right: -0.1em;
+  bottom: 0;
+  transition: top 200ms cubic-bezier(0, .8, .13, 1);
+  background-color: rgb(149, 85, 175, 0.502);
+}
+a:hover:after {
+  top: 0%;
 }
 </style>
